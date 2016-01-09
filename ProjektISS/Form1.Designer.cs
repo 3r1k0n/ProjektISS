99791a38c1c48ea221d86eb2943ab9047beccd3a
3r1k0n <3r1k0n@gmail.com>
2016-01-09 16:00:00 +0100

    na klik buttona se ucitaju 50 novih tocaka sinusoide

--- ProjektISS/Form1.Designer.cs
+++ ProjektISS/Form1.Designer.cs
@@ -28,21 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.SuspendLayout();
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.AutoScroll = true;
+            this.zedGraphControl1.BackColor = System.Drawing.SystemColors.Control;
+            this.zedGraphControl1.IsAntiAlias = true;
+            this.zedGraphControl1.IsEnableVPan = false;
+            this.zedGraphControl1.IsShowHScrollBar = true;
+            this.zedGraphControl1.IsShowPointValues = true;
+            this.zedGraphControl1.IsSynchronizeXAxes = true;
+            this.zedGraphControl1.Location = new System.Drawing.Point(27, 30);
+            this.zedGraphControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.Size = new System.Drawing.Size(824, 389);
+            this.zedGraphControl1.TabIndex = 0;
+            this.zedGraphControl1.UseExtendedPrintDialog = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(102, 426);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Calibri", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(917, 161);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(186, 148);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "00";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1148, 453);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Heart Rate";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(930, 501);
+            this.ClientSize = new System.Drawing.Size(1172, 477);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.zedGraphControl1);
+            this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
 
--- ProjektISS/Form1.cs
+++ ProjektISS/Form1.cs
@@ -1,5 +1,4 @@
-﻿using OxyPlot;
-using OxyPlot.Series;
+﻿using ZedGraph;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -9,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace ProjektISS
 {
@@ -17,9 +17,73 @@ namespace ProjektISS
         public Form1()
         {
             this.InitializeComponent();
-            var myModel = new PlotModel { Title = "Example 1" };
-            myModel.Series.Add(new FunctionSeries(Math.Cos, 0, 10, 0.1, "cos(x)"));
-            this.plot1.Model = myModel;
+        }
+
+        // Respond to form 'Load' event
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Setup the graph
+            CreateGraph(zedGraphControl1);
+
+        }
+
+        // Build the Chart
+        private void CreateGraph(ZedGraphControl zgc)
+        {
+            var document = XDocument.Load("HR.xml");
+            double[] Xarray = document.Descendants("X").Select(g => Math.Round((double)g,3)).ToArray();
+            double[] Yarray = document.Descendants("Y").Select(g => (double)g).ToArray();
+
+            // get a reference to the GraphPane
+            GraphPane myPane = zgc.GraphPane;
+
+            // Set the Titles
+            myPane.Title.Text = "Heart Rate";
+            myPane.XAxis.Title.Text = "seconds";
+            myPane.YAxis.Title.Text = "bpm";
+            myPane.Legend.IsVisible = false;
+
+            // Make up some data arrays based on the Sine function
+            double x, y1, y2;
+            PointPairList list1 = new PointPairList();
+            for (int i = 0; i < 36; i++)
+            {
+                x = (double)i;
+                y1 = 1.5 + Math.Sin((double)i * 0.2);
+                y2 = 3.0 * (1.5 + Math.Sin((double)i * 0.2));
+                list1.Add(x, y1);
+            }
+
+            // Generate a red curve with diamond
+            // symbols, and "Porsche" in the legend
+            LineItem myCurve = myPane.AddCurve("HR",
+                  list1, Color.Red, SymbolType.Square);
+            
+            // Tell ZedGraph to refigure the
+            // axes since the data have changed
+            zedGraphControl1.ScrollMinX = 0;
+            zgc.AxisChange();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double x, y1;
+            LineItem curve = zedGraphControl1.GraphPane.CurveList[0] as LineItem;
+            IPointListEdit list = curve.Points as IPointListEdit;
+
+            for (int i = 1; i < 50; i++)
+            {
+                //System.Threading.Thread.Sleep(500);
+                x = (int)list[list.Count - 1].X+ 1;
+                y1 = 1.5 + Math.Sin(0.2*x);
+                list.Add(x, y1);
+                zedGraphControl1.Invalidate();
+            }
+            zedGraphControl1.ScrollMaxX = list[list.Count - 1].X;
+
+            double width = zedGraphControl1.GraphPane.XAxis.Scale.Max - zedGraphControl1.GraphPane.XAxis.Scale.Min;
+            zedGraphControl1.GraphPane.XAxis.Scale.Max = list[list.Count - 1].X;
+            zedGraphControl1.GraphPane.XAxis.Scale.Min = zedGraphControl1.GraphPane.XAxis.Scale.Max-width;
         }
     }
 }
